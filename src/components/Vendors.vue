c1d3a4077b6e805888f53e6f3b776de862a5ac1f
tmferretti <michaelf@ritani.com>
2019-02-12 07:28:35 -0500

    Setup Proxy and getVendors

--- src/components/Vendors.vue
+++ src/components/Vendors.vue
@@ -3,12 +3,30 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Vendors',
   data () {
     return {
-      vendors: 'the vendors'
+      vendors: []
     }
+  },
+  methods: {
+    getVendors: function () {
+      let config = {
+        headers: {
+          Accept: 'application/json, application/vnd.negotiatus-challenge.com; version=1'
+        }
+      }
+      axios
+        .get('api/vendors', config)
+        .then(console.log(response))
+      return console.log(vendors)
+    }
+  },
+  mounted: function () {
+    this.getVendors();
   }
+
 }
 </script>
